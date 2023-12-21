7c340dc563c646d785a369ce7e0f44cfe9ad9144
Harsh <harshvardhan.vr946@gmail.com>
2023-12-21 15:17:58 -0500

    anagram

--- src/blind75/arrays/D_Easy_IsAnagram.java
+++ src/blind75/arrays/D_Easy_IsAnagram.java
@@ -1,2 +1,63 @@
-package blind75.arrays;public class D_Easy_IsAnagram {
+package blind75.arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ */
+public class D_Easy_IsAnagram {
+
+
+    //Time Complexity O(n2) - n is size of string, worst case scenario is strings have unique chars
+    //Space Complexity (n + m)
+    public boolean isAnagram_Brute(String s, String t) {
+
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        for(int i=0; i<s.length(); i++){
+            char sIdx = s.charAt(i);
+            char tIdx = t.charAt(i);
+
+            if(map1.containsKey(s.charAt(i))){
+                int cnt = map1.get(sIdx) +1;
+                map1.put(sIdx, cnt);
+            }else{
+                map1.put(sIdx, 1);
+            }
+
+            if(map2.containsKey(t.charAt(i))){
+                int cnt =  map2.get(tIdx) +1;
+                map2.put(tIdx, cnt);
+            }else{
+                map2.put(tIdx, 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : map1.entrySet()) {
+            char key = entry.getKey();
+            if(map2.get(key) ==null){
+                return false;
+            } else if(! map2.get(key).equals(entry.getValue())){
+                return false;
+            }
+        }
+
+        return true;
+
+
+    }
+
 }
