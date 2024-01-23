5fff74fe3f9a1a8f2d89a346f37d097491924201
Harsh <harshvardhan.vr946@gmail.com>
2024-01-22 22:32:37 -0500

    reverse LinkedList

--- src/blind75/datastructures/A_Easy_ReverseALinkedList.java
+++ src/blind75/datastructures/A_Easy_ReverseALinkedList.java
@@ -3,15 +3,20 @@ package blind75.datastructures;
 /**
  * Reverse a Linked List
  * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ *
  */
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
 public class A_Easy_ReverseALinkedList {
+
+    //Time Complexity O(n)
+    //Space complexity O(1)
     public ListNode A_Easy_ReverseALinkedList(ListNode head) {
 
         ListNode curr = head;
